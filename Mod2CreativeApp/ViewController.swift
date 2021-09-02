7f323846c5ca1a08a909795c397512bc44c3656b
Daniel Vega <daniel.vega23@d155.org>
2021-09-02 07:31:50 -0500

    Initial Commit

---
+++ Mod2CreativeApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Mod2CreativeApp
+//
+//  Created by DANIEL VEGA on 9/2/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
